fa8e314890072a69a3c9de06158ade9b0bd92c35
Andrii543 <andrii.rusyn15@gmail.com>
2024-12-19 15:12:28 +0100

    Fibonachi

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Home_Work_12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonachi(11));
+        }
+
+
+        static int Fibonachi(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+    }
+}
